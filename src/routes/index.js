0f7a7862198e5b92ed397515802bcfcecde2ab5f
Fantsy <fdwszd@163.com>
2022-09-17 09:13:56 +0800

    feat:路由

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,33 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+router.get("/", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "Hello Koa 2!",
+  });
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+router.get("/string", async (ctx, next) => {
+  ctx.body = "koa2 string";
+});
 
-router.get('/json', async (ctx, next) => {
+router.get("/json", async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
-
-module.exports = router
+    title: "koa2 json",
+  };
+});
+router.get("/profile/:userName", async (ctx, next) => {
+  const { userName } = ctx.params;
+  ctx.body = {
+    title: "this is a profile page",
+    userName,
+  };
+});
+router.get("/loadMore/:userName/:pageIndex", async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params;
+  ctx.body = {
+    title: "this is a loadMore page",
+    userName,
+    pageIndex,
+  };
+});
+module.exports = router;
