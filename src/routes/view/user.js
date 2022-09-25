80a0dab7480221b9745beaaad54f3483cf162dd3
Fantsy <fdwszd@163.com>
2022-09-25 20:32:54 +0800

    feat:判断是否已登录

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -1,11 +1,24 @@
 /**
  * @description 用户相关页面
  */
+function getUserInfo(ctx) {
+  let data = {
+    isLogin: false,
+  };
+  const userInfo = ctx.session.userInfo;
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName,
+    };
+  }
+  return data;
+}
 const router = require("koa-router")();
 router.get("/login", async (ctx, next) => {
-  await ctx.render("login", {});
+  await ctx.render("login", getUserInfo(ctx));
 });
 router.get("/register", async (ctx, next) => {
-  await ctx.render("register", {});
+  await ctx.render("register", getUserInfo(ctx));
 });
 module.exports = router;
